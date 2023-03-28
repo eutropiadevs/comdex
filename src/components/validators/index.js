afc0ab38cc0a0ad2f6188445818e86bc47ccb10e
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2023-03-28 23:58:49 +0530

    changes

--- src/components/validators/index.js
+++ src/components/validators/index.js
@@ -7,54 +7,55 @@ import rect from "../../assets/img/Rectangle 6985.png";
 
 const Validator = () => {
   return (
-    <>
-      <div className="validator-section">
-        <Head title={"Powering Comdex"} head={"TECHNOLOGY"} />
-        <div className="validator-section_main">
-          <div className="validator-section_main-first">
-            <img src={main} alt="img" className="img" />
-            <h2>
-              Built on Cosmos SDK
-              <br />
-              <span>&</span>
-              <br />
-              Interoperable Smart Contracts{" "}
-            </h2>
-            <p>
-              IBC-enabled DeFi infrastructure Layer for building an
-              interoperable ecosystem of solutions
-            </p>
-            <div className="btn">
-              <button>Learn More</button>
-            </div>
-          </div>
-          <div className="validator-section_main-second">
-            <div>
-              <h5>Validators</h5>
-              <hr />
-            </div>
-            <div className="validator-img">
-              {image.map((item) => {
-                return (
-                  <div key={item.id} className="image">
-                    <img src={item.src} />
-                  </div>
-                );
-              })}
-            </div>
-            <h5>+42 More</h5>
-            <div className="validator-section_main-second">
-              <div>
-                <h5>Security Audits</h5>
-                <hr />
-              </div>
-              <img src={rect} alt="img" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className="validator-section">
+				<Head title={"Powering Comdex"} head={"TECHNOLOGY"} />
+				<div className="validator-section_main">
+					<div className="validator-section_main-first">
+						<img src={main} alt="img" className="img" />
+						<h2>
+							Built on Cosmos SDK
+							<br />
+							<span>&</span>
+							<br />
+							Interoperable Smart Contracts{" "}
+						</h2>
+						<p>
+							IBC-enabled DeFi infrastructure Layer for building an
+							interoperable ecosystem of solutions
+						</p>
+						<div className="btn">
+							<button>Learn More</button>
+						</div>
+					</div>
+					<div className="vertical-line"></div>
+					<div className="validator-section_main-second">
+						<div>
+							<h5>Validators</h5>
+							<hr />
+						</div>
+						<div className="validator-img">
+							{image.map((item) => {
+								return (
+									<div key={item.id} className="image">
+										<img src={item.src} alt='img'/>
+									</div>
+								);
+							})}
+						</div>
+						<h5>+42 More</h5>
+						<div className="validator-section_main-second">
+							<div>
+								<h5>Security Audits</h5>
+								<hr />
+							</div>
+							<img src={rect} alt="img" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Validator;
